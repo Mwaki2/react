1961ade784a85c8458f93cce31b2aaf71aeea49a
mwaki <motleycrue.ms2@gmail.com>
2022-02-12 10:51:14 +0900

    main

--- chatbus/front/.eslintrc.js
+++ chatbus/front/.eslintrc.js
@@ -2,63 +2,32 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    "jest/globals": true
   },
   extends: [
     'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'prettier/@typescript-eslint'
+    'standard',
+    'prettier'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
-    ecmaVersion: 12,
-    sourceType: 'module',
-    tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json'],
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
   plugins: [
     'react',
     '@typescript-eslint',
-  ],
-  "ignorePatterns": [
-    ".eslintrc.js"
+    'jest'
   ],
   rules: {
-    'no-use-before-define': "off",
-    "@typescript-eslint/no-use-before-define": "off",
-    'import/prefer-default-export': "off",
-    'import/extensions': [
-        'error',
-        {
-          js: 'never',
-          jsx: 'never',
-          ts: 'never',
-          tsx: 'never',
-        },
-      ],
-      'react/jsx-filename-extension': [
-        'error',
-        {
-          extensions: ['.jsx', '.tsx'],
-        },
-      ],
-      'react/react-in-jsx-scope': 'off',
-      'no-void': [
-        'error',
-        {
-          allowAsStatement: true,
-        },
-      ],
+    'prettier/prettier': 'error'
   },
   settings: {
-    'import/resolver': {
-      node: {
-        paths: ['src'],
-        extensions: ['.js', '.jsx', '.ts', '.tsx']
-      },
-    },
-  },
-};
+    react: {
+      version: 'defect'
+    }
+  }
+}
